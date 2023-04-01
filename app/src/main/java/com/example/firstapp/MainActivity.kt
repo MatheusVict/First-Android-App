3652c98b4fafc05ffc2f3ae73d5fa19545298be9
MatheusVict <matheusvictorhenrique@gmail.com>
2023-04-01 17:08:48 -0300

    manipulating elements

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get elementeViewByiD And do whatever
+        val name = findViewById<TextView>(R.id.swCharchest)
+        /*name.text = "mudou?"*/
+
+        val inputChoose = findViewById<EditText>(R.id.inputText)
+        val btnSubmit = findViewById<Button>(R.id.btn)
+
+        // When button is clicked
+        btnSubmit.setOnClickListener {
+            if(inputChoose.text.toString().isEmpty()) {
+               inputChoose.error = getString(R.string.app_name)
+            } else {
+                name.text = "olá ${inputChoose.text}"
+            }
+        }
     }
 }
